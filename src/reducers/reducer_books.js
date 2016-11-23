021b518b81cd633caee686dbb783b88b7f700604
Harry Ferrier <f3rr13r@hotmail.com>
2016-11-23 12:18:31 +0000

    Push to git repository

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,24 @@
+export default function () {
+	return [
+		{ 
+			title: 'Javascript: The Good Parts',
+			author: 'Mr J. Script',
+			pages: 101 
+		},
+		{ 
+			title: 'Harry Potter',
+			author: 'JK Rowling',
+			pages: 390 
+		},
+		{ 
+			title: 'The Dark Tower',
+			author: 'Angus Tooley',
+			pages: 500
+		},
+		{ 
+			title: 'Eloquent Ruby',
+			author: 'Ruby Wax',
+			pages: 10 
+		}
+	];
+}
